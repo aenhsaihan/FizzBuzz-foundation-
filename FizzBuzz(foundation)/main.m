78f34b6bb13dfd85d4191b2d2b029880cc760060
Anar Enhsaihan <AnarE@Anars-MacBook-Pro.local>
2013-11-11 22:35:14 -0500

    Initial Commit

---
+++ FizzBuzz(foundation)/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  FizzBuzz(foundation)
+//
+//  Created by Anar Enhsaihan on 11/11/13.
+//  Copyright (c) 2013 Anar Enhsaihan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
